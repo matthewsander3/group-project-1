f7c2da1e0a5d60f51a5f047a22396c14d8e4db41
silvas123-cs <61157214+silvas123-cs@users.noreply.github.com>
2021-03-28 01:57:06 -0500

    MainMenu Addition
    I implemented the main menu

---
+++ Group Project/Assets/Scripts/LightControl.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightControl : MonoBehaviour
+{
+ 
+    void Update()
+    {
+        transform.Rotate(1,0,0 * Time.deltaTime);
+    }
+}
