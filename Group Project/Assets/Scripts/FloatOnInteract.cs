98837730521658e125794d3d278fb3c48dc6230f
cookiecait <caitlinmcoronado@gmail.com>
2021-05-05 03:49:19 -0500

    w

--- Group Project/Assets/Scripts/FloatOnInteract.cs
+++ Group Project/Assets/Scripts/FloatOnInteract.cs
@@ -11,23 +11,36 @@ public class FloatOnInteract : MonoBehaviour, IInteractable
     private float height;
     private float speed = 5f;
 <<<<<<< HEAD
+<<<<<<< HEAD
     Vector3 curPos ;
 =======
     Vector3 curPos;
 >>>>>>> Matt3
+=======
+    Vector3 curPos;
+>>>>>>> origin/Test
     Vector3 tempPos;
     Vector3 origPos;
 
     GameController gc;
-    private int cost = 10;
+    private int cost = 15;
+    private int scaredVal = 20;
     bool floating = false;
     bool ending = false;
+    bool scared = false;
     int i = 0;
+
+    public int redCol, blueCol, greenCol;
+    public bool flashingIn = false;
+    public bool lookingAt;
+
+    [SerializeField] private GameObject interactable;
 
     public void Start()
     {
         curPos = transform.position;
         origPos = curPos;
+        //interactable = GetComponent<GameController>().interactable; 
     }
 
     public void Update()
@@ -41,10 +54,14 @@ public class FloatOnInteract : MonoBehaviour, IInteractable
                     Debug.Log("Ending the loop");
                     ending = false;
 <<<<<<< HEAD
+<<<<<<< HEAD
                     
 =======
 
 >>>>>>> Matt3
+=======
+
+>>>>>>> origin/Test
                 }
             }
             else
@@ -56,51 +73,130 @@ public class FloatOnInteract : MonoBehaviour, IInteractable
             }
         }
 <<<<<<< HEAD
+<<<<<<< HEAD
    }
     public void OnStartHover()
     {
         Debug.Log("yeet ");
       
 =======
+=======
     }
+
+    IEnumerator OnLookFlash()
+    {
+        while (lookingAt == true)
+        {
+            yield return new WaitForSeconds(0.05f);
+            if (flashingIn)
+            {
+                if (blueCol <= 30)
+                {
+                    flashingIn = false;
+                }
+                else
+                {
+                    blueCol -= 25;
+                    redCol -= 25;
+                    //greenCol -= 25;
+                }
+            }
+
+            if (!flashingIn)
+            {
+                if (blueCol >= 250)
+                {
+                    flashingIn = true;
+                }
+                else
+                {
+                    blueCol += 25;
+                    redCol += 25;
+                    //greenCol += 25;
+                }
+            }
+            gameObject.GetComponent<Renderer>().material.color = new Color32((byte)redCol, (byte)greenCol, (byte)blueCol, 255);
+        }
+>>>>>>> origin/Test
+    }
+
     public void OnStartHover()
     {
         Debug.Log("yeet ");
+        lookingAt = true;
+        interactable.SetActive(true);
+        gameObject.GetComponent<Renderer>().material.color = new Color32((byte)redCol, (byte)greenCol, (byte)blueCol, 255);
+        StartCoroutine("OnLookFlash");
 
+<<<<<<< HEAD
 >>>>>>> Matt3
+=======
+>>>>>>> origin/Test
     }
     public void OnInteract()
     {
         StartCoroutine("Float");
         GameController.cost = cost;
+        //didScared()           call this function to see if you scared the person and if yes then change scared bool to true
+        if (scared)             //
+        {
+            Debug.Log("In range!");
+            GameController.scareVal = scaredVal;
+            scared = false;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        scared = true;
+    }
+
+    public void didScare()
+    {
+        //if()
     }
 
     IEnumerator Float()
     {
 <<<<<<< HEAD
+<<<<<<< HEAD
         
 =======
 
 >>>>>>> Matt3
+=======
+
+>>>>>>> origin/Test
         floating = true;
         yield return new WaitForSeconds(3);
         ending = true;
         yield return new WaitForSeconds(1);
         floating = false;
 <<<<<<< HEAD
+<<<<<<< HEAD
       //  transform.position = origPos;
 =======
         //  transform.position = origPos;
 >>>>>>> Matt3
+=======
+        //  transform.position = origPos;
+>>>>>>> origin/Test
     }
     public void OnEndHover()
     {
         Debug.Log("going out");
 <<<<<<< HEAD
+<<<<<<< HEAD
         
 =======
 
 >>>>>>> Matt3
+=======
+        interactable.SetActive(false);
+        lookingAt = false;
+        StopCoroutine("OnLookFlash");
+        gameObject.GetComponent<Renderer>().material.color = new Color32(255, 255, 255, 255);
+>>>>>>> origin/Test
     }
 
 
