3567eef10d1b65bb42e6142e7f1f8fc76cf14575
silvas123-cs <61157214+silvas123-cs@users.noreply.github.com>
2021-05-04 22:39:02 -0500

    Floats

--- Group Project/Assets/Scripts/FloatOnInteract.cs
+++ Group Project/Assets/Scripts/FloatOnInteract.cs
@@ -4,21 +4,79 @@ using UnityEngine;
 
 public class FloatOnInteract : MonoBehaviour, IInteractable
 {
-    public float MaxRange => throw new System.NotImplementedException();
+    public float MaxRange { get { return maxRange; } }
 
+    private const float maxRange = 10f;
 
+    private float height;
+    private float speed = 5f;
+    Vector3 curPos;
+    Vector3 tempPos;
+    Vector3 origPos;
+
+    GameController gc;
+    private int cost = 10;
+    bool floating = false;
+    bool ending = false;
+    int i = 0;
+
+    public void Start()
+    {
+        curPos = transform.position;
+        origPos = curPos;
+    }
+
+    public void Update()
+    {
+        if (floating)
+        {
+            if (ending)
+            {
+                if ((1 + Mathf.Sin(Time.time * Mathf.PI)) * 0.5f < 0.5)
+                {
+                    Debug.Log("Ending the loop");
+                    ending = false;
+
+                }
+            }
+            else
+            {
+                tempPos = curPos;
+                tempPos.y += (1 + Mathf.Sin(Time.time * Mathf.PI)) * 0.5f;
+                //Debug.Log((1 + Mathf.Sin(Time.time * Mathf.PI)) * 0.5f);
+                transform.position = tempPos;
+            }
+        }
+    }
     public void OnStartHover()
     {
-        throw new System.NotImplementedException();
+        Debug.Log("yeet ");
+
     }
     public void OnInteract()
     {
-        throw new System.NotImplementedException();
+        StartCoroutine("Float");
+        GameController.cost = cost;
+    }
+
+    IEnumerator Float()
+    {
+
+        floating = true;
+        yield return new WaitForSeconds(3);
+        ending = true;
+        yield return new WaitForSeconds(1);
+        floating = false;
+        //  transform.position = origPos;
     }
     public void OnEndHover()
     {
-        throw new System.NotImplementedException();
+        Debug.Log("going out");
+
     }
 
 
 }
+
+
+
